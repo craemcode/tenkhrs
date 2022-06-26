b14a29b306db9aafdc72d6c42731240c9c6fa1a2
Zachariah <ndunguzack13@gmail.com>
2022-06-26 17:12:49 +0300

    plugs db

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 from flask import Flask, render_template
+from models import db, Plug, Product
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SECRET_KEY'] = 'JABAWABA2'
+db.init_app(app)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)
+
+
 @app.route('/')
 def index():
+    
+    
+    
+    
     return render_template('domanip.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
